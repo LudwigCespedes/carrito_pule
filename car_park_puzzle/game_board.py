05eab7883a35eb05761f815e3c84d96bd4797d75
Ludwig Céspedes Ogando <ludwigcespedesogando@gmail.com>
2024-06-01 19:38:41 -0400

    Agregando bfs

--- car_park_puzzle/game_board.py
+++ car_park_puzzle/game_board.py
@@ -1,26 +1,27 @@
 
+from collections import deque
+
+class Car:
+    """
+    A class for modeling a car 
+    """
+    def __init__(self, id, length, orientation, row, col):
+        self.id = id
+        self.length = length
+        self.orientation = orientation
+        self.row = row
+        self.col = col
+
+
 class Board:
     def __init__(self, row, col):
-        """
-        Initializes a game board with the given number of rows and columns.
-
-        :param row: Number of rows in the board.
-        :param col: Number of columns in the board.
-        """
-
         self.row = row
         self.col = col
         self.board = [['.' for _ in range(col)] for _ in range(row)]
         self.cars = {}
 
     def add_car(self, car):
-        """
-        Adds a car to the board and places it at the specified position based on its orientation.
-
-        :param car: An object representing the car with attributes id, orientation, length, row, and col.
-        """
         self.cars[car.id] = car
-        # Check if the car can be placed without exceeding boundaries
         if car.orientation == 'H':
             for i in range(car.length):
                 self.board[car.row][car.col + i] = car.id
@@ -32,16 +33,7 @@ class Board:
         for row in self.board:
             print(' '.join(row))
         
-
     def move_car(self, car_id, direction, steps):
-        """
-        Moves a car on the board in the specified direction by the specified number of steps.
-
-        :param car_id: The identifier of the car to be moved.
-        :param direction: The direction to move the car ('A' for left, 'D' for right, 'W' for up, 'S' for down).
-        :param steps: The number of steps to move the car.
-        """
-
         car = self.cars[car_id]
         if car.orientation == 'H':
             if direction == 'A':
@@ -52,7 +44,7 @@ class Board:
                     for i in range(car.length):
                         self.board[car.row][car.col + i] = car.id
             elif direction == 'D':
-                if car.col + car.length + steps - 1 < self.row and all(self.board[car.row][car.col + car.length - 1 + i] == '.' for i in range(1, steps + 1)):
+                if car.col + car.length + steps - 1 < self.col and all(self.board[car.row][car.col + car.length - 1 + i] == '.' for i in range(1, steps + 1)):
                     for i in range(car.length):
                         self.board[car.row][car.col + i] = '.'
                     car.col += steps
@@ -74,36 +66,18 @@ class Board:
                     for i in range(car.length):
                         self.board[car.row + i][car.col] = car.id
 
-
-
-#    def check_victory(self, car_id, exit_row, exit_col):
-#        car = self.cars[car_id]
-#        if car.orientation == 'H' and car.col + car.length - 1 == exit_col and car.row == exit_row:
-#            return True
-#       return False
-    
     def check_victory(self, car_id, exit_row, exit_col):
-        """
-        Checks if the specified car has reached the exit position on the board.
-
-        :param car_id: The identifier of the car to check.
-        :param exit_row: The row of the exit position.
-        :param exit_col: The column of the exit position.
-        :return: True if the car has reached the exit position, False otherwise.
-        """
         car = self.cars[car_id]
-        
         if car.orientation == 'H':
             if car.col + car.length - 1 == exit_col and car.row == exit_row:
                 return True
         elif car.orientation == 'V':
             if car.row + car.length - 1 == exit_row and car.col == exit_col:
                 return True
-        
         return False
+
     def get_movable_cars(self):
         movable_cars = []
-
         for car_id, car in self.cars.items():
             if car.orientation == 'H':
                 for steps in range(1, car.col + 1):
@@ -129,26 +103,35 @@ class Board:
                         break
         return movable_cars
 
-    def bfs (self):
-        frontier = self.get_movable_cars()
-        explored = []
-        q = True
-        while q and len(frontier)!=0:
-            #print(f'frontera: {frontier}')
-            #print(f'explored: {explored}')
-            node = frontier.pop(0)
-            if self.check_victory('1', 2, 5):
-                print(f"haz ganado")
-                q = False
-                break
-            
-            if node not in explored:
-                explored.append(node)
-                try:
-                    self.move_car(node[0], node[1], node[2])
-                except Exception as e:
-                    print(f"Error al mover el carro {node}: {e}")
-                    continue
+    def copy_board(self):
+        new_board = Board(self.row, self.col)
+        new_board.board = [row[:] for row in self.board]
+        new_board.cars = {car_id: Car(car.id, car.length, car.orientation, car.row, car.col) for car_id, car in self.cars.items()}
+        return new_board
 
-                frontier.append(self.get_movable_cars())
-            self.display_board()
+    def bfs(self, target_car_id, exit_row, exit_col):
+        initial_state = self.copy_board()
+        frontier = deque([(initial_state, [])])
+        visited = set()
+
+        while frontier:
+            current_state, path = frontier.popleft()
+
+            if current_state.check_victory(target_car_id, exit_row, exit_col):
+                print(f"Ganaste. Ruta: {path}")
+                return path
+
+            current_state_str = str(current_state.board)
+            if current_state_str in visited:
+                continue
+
+            visited.add(current_state_str)
+            for move in current_state.get_movable_cars():
+                new_state = current_state.copy_board()
+                new_state.move_car(move[0], move[1], move[2])
+                new_path = path + [move]
+                frontier.append((new_state, new_path))
+
+        print("No se encontró solución.")
+        return None
+
--- main.py
+++ main.py
@@ -1,34 +1,42 @@
-from car_park_puzzle.car import Car
-from car_park_puzzle.game_board import Board
-from car_park_puzzle.gui.main_gui import *
+from car_park_puzzle.game_board import Board , Car
 
-#main()
+"""
+        car_id = input("Ingresa el ID del coche a mover: ")
+        direction = input("Ingresa la dirección (A, D, W, S): ")
+        steps = int(input("Ingresa el número de pasos: "))
+
+        game.move_car(car_id, direction, steps)
+
+        if game.check_victory('1', 2, 5):
+            print("¡Has ganado!")
+            break
+"""
+
 def main():
-    CAR1 = Car('1', 2, 'H', 2, 2)
-    CAR2 = Car('2', 2, 'H', 3, 3)
-    CAR3 = Car('3', 3, 'V', 0, 0)
-    CAR4 = Car('4', 2, 'H', 0, 3)
-    CAR5 = Car('5', 2, 'V', 3, 1)
-    CAR6 = Car('7', 5, 'H', 5, 0)
-    game = Board(row=6, col=6)
-    game.add_car(CAR1)
-    game.add_car(CAR2)  # Coche objetivo
-    game.add_car(CAR3)
-    game.add_car(CAR3)
-    game.add_car(CAR4)
-    game.add_car(CAR5)
-    game.add_car(CAR6)
+    tablero = Board(5, 7)
 
+    # Agregar los coches
+    tablero.add_car(Car('A', 2, 'H', 2, 0))
+    #tablero.add_car(Car('1', 2, 'V', 2, 5))
+    tablero.add_car(Car('2', 2, 'V', 3, 0))
+    tablero.add_car(Car('3', 2, 'V', 2, 3))
+    tablero.add_car(Car('4', 2, 'H', 4, 2))
+    tablero.add_car(Car('5', 2, 'V', 2, 5))
+    tablero.add_car(Car('6', 2, 'V', 3, 6))
+    tablero.add_car(Car('7', 2, 'V', 1, 6))
+    tablero.add_car(Car('8', 2, 'H', 0, 1))
+    tablero.add_car(Car('9', 2, 'H', 0, 5))
+    tablero.add_car(Car('S', 2, 'H', 1, 2))
+    tablero.add_car(Car('C', 2, 'H', 3, 1))
+    tablero.add_car(Car('B', 2, 'H', 4, 4))
 
-    #while True:
-    game.display_board()
-    print(game.get_movable_cars().append(game.get_movable_cars()))
-    print("Estado inicial del tablero:")
-    game.display_board()
+    # Mostrar el tablero inicial
+    tablero.display_board()
 
-    game.bfs()
-        
-        
+    # Ejecutar BFS para encontrar la solución
+    t=tablero.bfs('A', 2, 6)
+    tablero.display_board()
+    print(t)  
    
 """
         car_id = input("Ingresa el ID del coche a mover: ")
@@ -46,3 +54,5 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+
