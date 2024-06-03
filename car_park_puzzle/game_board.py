381bfba4c59c661eaefaa61a7e83fd9721d5d1f3
Ludwig Céspedes Ogando <ludwigcespedesogando@gmail.com>
2024-06-03 16:14:48 -0400

    porfin acabe esta mierda

--- car_park_puzzle/game_board.py
+++ car_park_puzzle/game_board.py
@@ -1,4 +1,4 @@
-
+import heapq
 from collections import deque
 
 class Car:
@@ -142,13 +142,91 @@ class Board:
         print("No se encontró solución.")
         return None
     
-    def dfs1(self, target_car_id, exit_row, exit_col):
+    def dfs(self, target_car_id, exit_row, exit_col,max_depth=10):
         initial_state = self.copy_board()
-        frontier = [(initial_state, [])]
+        frontier = [(initial_state, [], 0)]
         visited = set()
-        #print(frontier.popleft())
+        #print(frontier.pop())
         while frontier:
-            current_state, path = frontier.pop()
+            current_state, path, depth = frontier.pop()
+            if depth > max_depth:
+                continue
+            if current_state.check_victory(target_car_id, exit_row, exit_col):
+                print(f"Ganaste. Ruta: {path}")
+                return path
+
+            current_state_str = str(current_state.board)
+            if current_state_str in visited:
+                continue
+
+            visited.add(current_state_str)
+            for move in current_state.get_movable_cars():
+                new_state = current_state.copy_board()
+                new_state.move_car(move[0], move[1], move[2])
+                new_path = path + [move]
+                frontier.append((new_state, new_path, depth+1))
+
+        print("No se encontró solución.")
+        return None
+
+    def heuristic_manhattan(self, car, exit_row, exit_col):
+        if car.orientation == 'H':
+            return abs(exit_row - car.row) + abs(exit_col - (car.col + car.length - 1))
+        elif car.orientation == 'V':
+            return abs(exit_row - (car.row + car.length - 1)) + abs(exit_col - car.col)
+
+    def heuristic_euclidean(self, car, exit_row, exit_col):
+        if car.orientation == 'H':
+            return ((exit_row - car.row)**2 + (exit_col - (car.col + car.length - 1))**2)**0.5
+        elif car.orientation == 'V':
+            return ((exit_row - (car.row + car.length - 1))**2 + (exit_col - car.col)**2)**0.5
+
+    def heuristic_blocking_cars(self, car, exit_row, exit_col):
+        blocking_count = 0
+        if car.orientation == 'H':
+            for col in range(car.col + car.length, self.col):
+                if self.board[car.row][col] != '.':
+                    blocking_count += 1
+        elif car.orientation == 'V':
+            for row in range(car.row + car.length, self.row):
+                if self.board[row][car.col] != '.':
+                    blocking_count += 1
+        return blocking_count
+
+    def heuristic_weighted_manhattan(self, car, exit_row, exit_col):
+        blocking_cars = self.heuristic_blocking_cars(car, exit_row, exit_col)
+        return self.heuristic_manhattan(car, exit_row, exit_col) + blocking_cars
+
+    def heuristic_movement_distance(self, car, exit_row, exit_col):
+        if car.orientation == 'H':
+            return abs(exit_col - (car.col + car.length - 1))
+        elif car.orientation == 'V':
+            return abs(exit_row - (car.row + car.length - 1))
+
+    def combined_heuristics(self, car, exit_row, exit_col, heuristics):
+        total_cost = 0
+        for heuristic in heuristics:
+            if heuristic == 'manhattan':
+                total_cost += self.heuristic_manhattan(car, exit_row, exit_col)
+            elif heuristic == 'euclidean':
+                total_cost += self.heuristic_euclidean(car, exit_row, exit_col)
+            elif heuristic == 'blocking_cars':
+                total_cost += self.heuristic_blocking_cars(car, exit_row, exit_col)
+            elif heuristic == 'weighted_manhattan':
+                total_cost += self.heuristic_weighted_manhattan(car, exit_row, exit_col)
+            elif heuristic == 'movement_distance':
+                total_cost += self.heuristic_movement_distance(car, exit_row, exit_col)
+        return total_cost
+
+    def a_star(self, target_car_id, exit_row, exit_col, heuristics):
+        initial_state = self.copy_board()
+        frontier = []
+        heapq.heappush(frontier, (0, 0, initial_state, []))
+        visited = set()
+        state_id = 0
+
+        while frontier:
+            cost, _, current_state, path = heapq.heappop(frontier)
 
             if current_state.check_victory(target_car_id, exit_row, exit_col):
                 print(f"Ganaste. Ruta: {path}")
@@ -163,34 +241,11 @@ class Board:
                 new_state = current_state.copy_board()
                 new_state.move_car(move[0], move[1], move[2])
                 new_path = path + [move]
-                frontier.append((new_state, new_path))
+                heuristic_cost = new_state.combined_heuristics(self.cars[target_car_id], exit_row, exit_col, heuristics)
+                total_cost = cost + 1 + heuristic_cost
+                state_id += 1
+                heapq.heappush(frontier, (total_cost, state_id, new_state, new_path))
 
         print("No se encontró solución.")
         return None
-
-def dfs(board, car_id, goal_row, goal_col, max_depth=1000):
-    stack = [(board, [], 0)]  # (tablero actual, movimientos, profundidad)
-    visited = set()
-
-    while stack:
-        current_board, path, depth = stack.pop()
-
-        if depth > max_depth:
-            continue
-
-        current_state = current_board.get_state()
-        if current_state in visited:
-            continue
-
-        visited.add(current_state)
-
-        if current_board.check_victory(car_id, goal_row, goal_col):
-            print(f"¡Ganador! Carro {car_id} alcanzó la meta.")
-            return path
-
-        for move in current_board.get_possible_moves():
-            next_board = current_board.copy()
-            next_board.move_car(move)
-            stack.append((next_board, path + [move], depth + 1))
-
-    return None
+ 
